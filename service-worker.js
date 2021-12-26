731474fd4d6c86bf859ae2d2b66978620fdc4751
polycrates <davidc@uoregon.edu>
2021-12-26 22:22:32 +0000

    Deploying to gh-pages from  @ 2006bbeadffaf4c6c5e86ce7979154bf91027550 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/treebank-template/precache-manifest.36b631372c8bb0db04918c7b3cfa47f2.js"
+  "/treebanks/precache-manifest.49af89cdbadc62d88853c02e298535e5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/treebank-template/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/treebanks/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
